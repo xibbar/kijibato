7eecd86804f93f5c806542e0f203dc8ee16f1126
Takeyuki FUJIOKA <fujioka@rabbix.net>
2014-06-19 10:21:04 +0900

    basic認証をつけた。パスワードは内緒。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def authenticate
+    authenticate_or_request_with_http_basic('Enter Password') do |u, p|
+      u == 'admin' && Digest::MD5.hexdigest(p) == "d32452cb867fafec6179158051e9c980" 
+    end
+  end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :authenticate
   before_action :set_group, only: [:show, :edit, :update, :destroy]
 
   # GET /groups

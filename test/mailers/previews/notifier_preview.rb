dcc14597e28dcb83dd68acdde0493c78a4996959
Takeyuki FUJIOKA <fujioka@rabbix.net>
2014-06-24 14:41:22 +0900

    testを削除

--- test/mailers/previews/notifier_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/notifier
-class NotifierPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/notifier/article
-  def article
-    Notifier.article
-  end
-
-end
